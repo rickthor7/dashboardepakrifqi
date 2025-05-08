9ccf55f00b44e2152b8008a087d2a39eee686364
Gizellemathilda <gizellemmamentu@gmail.com>
2025-05-08 21:19:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'baru1.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.get('/test', (req, res) => {
